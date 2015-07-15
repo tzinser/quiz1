cf0bb265e2ff23f723dccca5059b1e494eebc4eb
Tina Zinser <zinsert7@gmail.com>
2015-07-15 17:36:31 +0000

    Add simple form gem, create new form to accept last name and first name, add to database

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,17 @@ class QuizzesController < ApplicationController
 	def index
 		@quizzes = Quiz.all
 	end
+
+	def new
+		@quiz = Quiz.new
+	end
+
+	def create
+		Quiz.create(quiz_params)
+		redirect_to root_path
+	end
+
+	def quiz_params
+		params.require(:quiz).permit(:lastname, :firstname)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Quiz1::Application.routes.draw do
   root 'quizzes#index'
+  resources :quizzes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
